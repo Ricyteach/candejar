34942e2eab2059b438da687fc5147626b0c6dd72
Rick Teachey <ricky@teachey.org>
2018-03-20 00:52:47 -0400

    Added dataclasses to requirements (Python 3.7 only)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ['dataclasses', ]
 
 setup_requirements = ['pytest-runner', ]
 
