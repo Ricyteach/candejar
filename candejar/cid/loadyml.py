f39bdece52751e7edfa14546abdbc7d048ec47e0
Rick Teachey <ricky@teachey.org>
2018-03-21 17:40:37 -0400

    Fixed STUPID PyYAML unicode problem by replacing phi symbols after incorrectly loaded

--- candejar/cid/loadyml.py
+++ candejar/cid/loadyml.py
@@ -7,11 +7,11 @@ from pathlib import Path
 def load_yml_objs():
     with Path(__file__).resolve().parents[1].joinpath("cid/ciddefs.yml").open() as f:
         objs = list(yaml.safe_load_all(f))
-    o = objs[-1][1]
-    if any("Ï•" in k for d in o.values() for k in d.keys()):
-        d_replace = dict()
-        for dname,d in o.items():
-            for k in d.keys():
-                k_new = k.replace("Ï•", "ϕ")
-
+    pipes = objs[-1][1]
+    if any("Ï•" in k for d in pipes.values() for k in d.keys()):
+        pipes_replace = dict()
+        for dname,d in pipes.items():
+            d_new = {k.replace("Ï•", "φ"):v for k,v in d.items()}
+            pipes_replace[dname] = d_new
+        objs[-1][1] = pipes_replace
     return objs
