82a5d653a03d0a4757245a0bda2099ef70fd066d
Rick Teachey <ricky@teachey.org>
2018-03-21 17:30:33 -0400

    Trying to fix STUPID PyYAML unicode problem

--- candejar/cid/loadyml.py
+++ candejar/cid/loadyml.py
@@ -7,4 +7,11 @@ from pathlib import Path
 def load_yml_objs():
     with Path(__file__).resolve().parents[1].joinpath("cid/ciddefs.yml").open() as f:
         objs = list(yaml.safe_load_all(f))
+    o = objs[-1][1]
+    if any("Ï•" in k for d in o.values() for k in d.keys()):
+        d_replace = dict()
+        for dname,d in o.items():
+            for k in d.keys():
+                k_new = k.replace("Ï•", "ϕ")
+
     return objs
