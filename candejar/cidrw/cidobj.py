ec4319c91212dfe7e970c896e1d32591ef68bea4
Rick Teachey <ricky@teachey.org>
2018-03-23 17:17:09 -0400

    Continuing to work on cidobj module

--- candejar/cidrw/cidobj.py
+++ candejar/cidrw/cidobj.py
@@ -3,6 +3,7 @@
 """CID object module for working with an entire cid file as a read/write object."""
 from dataclasses import dataclass, field
 from typing import List, Any, ClassVar
+from ..cid.cidlineclasses import E1
 from ..cid.cidline import CidLine
 
 
@@ -23,11 +24,32 @@ class AttributeDelegator:
         setattr(delegate, self.name, value)
 
 @dataclass
+class PipeGroup:
+    # only input parameter is lines
+    line_objs: List[CidLine] = field(default_factory=list, repr=False)  # cid file line objects
+
+    # all other fields are for display/output
+    type_: str = field(default=AttributeDelegator("a2"), init=False)  # ALUMINUM, BASIC, CONCRETE, PLASTIC, STEEL,
+                                                                      # CONRIB, CONTUBE
+
+
+@dataclass
+class Material:
+    # only input parameter is lines
+    line_obj: List[CidLine] = field(default_factory=list, repr=False)  # cid file line objects
+
+    # all other fields are for display/output
+    model: int = field(default=AttributeDelegator("d1"), init=False)  # 1: Isotropic, 2: Orthotropic, # 3: Duncan/Selig,
+                                                                      # 4: Overburden, # 5: Extended Hardin,
+                                                                      # 6: Interface, 7: Composite Link, 8: Mohr/Coulomb
+
+
+@dataclass
 class CidObjIn:
     # only input parameter is lines
-    lines: List[CidLine] = field(default_factory=list, repr=False)  # cid file lines
+    line_objs: List[CidLine] = field(default_factory=list, repr=False)  # cid file line objects
 
-    # all other fields are for display
+    # all other fields are for display/output
     mode: int = field(default=AttributeDelegator("a1"), init=False)  # ANALYS or DESIGN
     level: int = field(default=AttributeDelegator("a1"), init=False)  # 1, 2, 3
     method: int = field(default=AttributeDelegator("a1"), init=False)  # 0=WSD, 1=LRFD
@@ -38,14 +60,14 @@ class CidObjIn:
     nboundaries: int = field(default=AttributeDelegator("c2"), init=False)
     nsoilmaterials: int = field(default=AttributeDelegator("c2"), init=False)
     ninterfmaterials: int = field(default=AttributeDelegator("c2"), init=False)
-    groups: List[Any] = field(default_factory=list, init=False)  # pipe groups
+    groups: List[PipeGroup] = field(default_factory=list, init=False)  # pipe groups
     """
     nodes: List[Any] = field(default_factory=list, init=False)
     elements: List[Any] = field(default_factory=list, init=False)
     boundaries: List[Any] = field(default_factory=list, init=False)
     """
-    materials: List[Any] = field(default_factory=list, init=False)  # element materials
-    factors: List[Any] = field(default_factory=list, init=False)  # lrfd step factors
+    materials: List[Material] = field(default_factory=list, init=False)  # element materials
+    factors: List[E1] = field(default_factory=list, init=False)  # lrfd step factors
 
     @property
     def soilmaterials(self):
