d2b1523390f66e3060931bdde5a0e35eed4e15d3
Rick Teachey <ricky@teachey.org>
2018-03-26 01:31:04 -0400

    Add loading input_test.cid file text to test_cidobj

--- tests/test_cidobj.py
+++ tests/test_cidobj.py
@@ -4,3 +4,12 @@
 """Tests for `candejar.cidobj` module."""
 
 import pytest
+from pathlib import Path
+from candejar.cidrw.cidobj import CidObjIn
+
+@pytest.fixture
+def cid_file_lines():
+    return Path(__file__).resolve().parents[0].joinpath("input_test.cid").read_text().split("\n")
+
+def test_new_cid_obj(cid_file_lines):
+    assert CidObjIn(cid_file_lines)
