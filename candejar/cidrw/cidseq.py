bbadf60359cc321561d1e4a0a4b24c68f2aa81e0
Rick Teachey <ricky@teachey.org>
2018-03-30 17:17:04 -0400

    in process of fixing problem with `make_dataclass`, type variables, and supplying the dynamic method to the new class

--- candejar/cidrw/cidseq.py
+++ candejar/cidrw/cidseq.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """CID sequence module for working with CID sub object sequences (pipe groups, nodes, etc)."""
-from abc import abstractmethod, ABC
-from dataclasses import dataclass, field, InitVar, make_dataclass
-from typing import Sequence, Generic, Type, MutableSequence, Iterator, Union, TypeVar, Optional
+from abc import abstractmethod
+from dataclasses import field, InitVar, make_dataclass
+from typing import Sequence, Generic, Type, Iterator, Union, TypeVar, Optional
 
 from .. import fea
 from ..cid import CidSubLine
-from ..utilities.collections import ChainSequence
 from ..utilities.mixins import ChildRegistryBase
 from .cidsubobj import CidSubObj
 from .exc import CIDRWError
@@ -21,7 +20,7 @@ class CIDSubSeqError(CIDRWError):
 CidObj = TypeVar("CidObj", covariant=True)
 
 
-class CidSeq(ABC, ChildRegistryBase, Sequence[CidSubObj[CidObj, "CidSeq", CidSubLine, fea.FEAObj]], Generic[CidObj, CidSubLine, fea.FEAObj]):
+class CidSeq(ChildRegistryBase, Sequence[CidSubObj[CidObj, "CidSeq", CidSubLine, fea.FEAObj]], Generic[CidObj, CidSubLine, fea.FEAObj]):
 
     def __post_init__(self, seq_name: str) -> None:
         if getattr(self.cid_obj, seq_name):
@@ -53,8 +52,7 @@ class CidSeq(ABC, ChildRegistryBase, Sequence[CidSubObj[CidObj, "CidSeq", CidSub
             self.add_new(obj)
 
     @abstractmethod
-    def add_new(self, obj: CidSubObj[CidObj, "CidSeq", CidSubLine, fea.FEAObj]) -> None:
-        self.seq.append(obj)
+    def add_new(self, obj: CidSubObj[CidObj, "CidSeq", CidSubLine, fea.FEAObj]) -> None: ...
 
     def __getitem__(self, val: Union[slice, int]) -> CidSubObj[CidObj, "CidSeq", CidSubLine, fea.FEAObj]:
         try:
@@ -73,8 +71,6 @@ class CidSeq(ABC, ChildRegistryBase, Sequence[CidSubObj[CidObj, "CidSeq", CidSub
             return len(s)
 
 
-
-
 def subclass_CidSeq(seq_name):
     """Produce a `CidSeq` based subclass `dataclass`."""
 
@@ -85,25 +81,34 @@ def subclass_CidSeq(seq_name):
     except KeyError:
         pass
 
-    # resolvea all the types
+    # resolve all the types
     SubLine = SEQ_NAME_DICT[seq_name]  # type of CidLine indicating start of an object in CID file
     FEA_Obj = TYPE_DICT[SEQ_NAME_DICT[seq_name]]  # type of FEA object corresponding to CID object
-    CidSeqChild = TypeVar("CidSeqChild", bound=CidSeq[CidObj, SubLine, FEA_Obj])
+    CidSeqChild = CidSeq[CidObj, SubLine, FEA_Obj]
     SubObj = CidSubObj[CidObj, CidSeqChild, SubLine, FEA_Obj]
 
     # provide alternate new object insertion for soil materials (needs to be insert into middle
     # of the `ChainSequence` container).
-    def add_new_soilmaterial(self, material: CidSubObj[CidObj, CidSeqChild, SubLine, FEA_Obj]) -> None:
+    def add_new_soilmaterial(self, material: SubObj) -> None:
         self.seq.sequences[0].append(material)
+
+    # default new object insertion for all other `CidSeq` children
+    def add_new_(self, item: SubObj) -> None:
+        self.seq.append(item)
 
     # lookup correct `add_new` method
     add_new_dict = dict(soilmaterials=add_new_soilmaterial)
-    add_new = add_new_dict.get(seq_name, CidSeq.add_new)
+    add_new_method = add_new_dict.get(seq_name, add_new_)
+
+    # TODO: this will not work as-is because `make_dataclass` calls `type` with the namespace instead of `types.new_class`
+    # Changing to `types.new_class` creates a new problem because `make_dataclass` doesn't know to utilize exec_body
 
     cls = make_dataclass(cls_name,
                          (("cid_obj", CidObj, field(repr=False)),
                           ("seq_name", InitVar[str]),
                           ("line_type", Type[SubLine], field(default=SubLine, init=False, repr=False)),
                           ("seq", Optional[Sequence[SubObj]], field(default=None, init=False))),
-                         bases = (CidSeqChild, Generic[CidObj]), namespace = dict(add_new=add_new))
+                         bases = (CidSeqChild, Generic[CidObj]),
+                         namespace = dict(), eq=False)
+
     return cls
