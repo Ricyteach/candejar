11096672650cca3d64eb6cfbbbb12d83f2cf5d81
Rick Teachey <ricky@teachey.org>
2018-04-04 13:13:16 -0400

    Some helpful tools for usage with `dataclass` objects

---
+++ candejar/utilities/dataclasses.py
@@ -0,0 +1,25 @@
+from dataclasses import is_dataclass, asdict, fields
+from typing import Any, Mapping, Callable, Tuple
+
+
+def mapify(o: Any) -> Mapping:
+    """Convert an object so it can be unpacked as **kwargs to another context."""
+    if is_dataclass(o):
+        return asdict(o)
+    elif isinstance(o, Mapping):
+        return o
+    else:
+        if isinstance(o, type):
+            raise TypeError("Received command to turn the class instance of {o.__name__!s} to a mapping")
+        else:
+            return vars(o)
+
+def unmapify(d: Mapping, f: Callable, key_validator: Callable[[Any], bool]=lambda k: True):
+    """Feed a mapping to a function using only validated keys."""
+    return f(**{k:v for k,v in d.items() if key_validator(k)})
+
+def field_names(cls_or_instance) -> Tuple[str, ...]:
+    """Return a tuple of field names for this dataclass.
+
+    Accepts a dataclass or an instance of one. Tuple elements are of type `dataclasses.Field`."""
+    return tuple(f.name for f in fields(cls_or_instance))
