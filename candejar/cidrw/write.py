eb9f82cb96f4f6cc078c90b25c0b204a9b234a9a
Rick Teachey <ricky@teachey.org>
2018-04-04 13:16:25 -0400

    wrote logic for writing a vanilla cid type of object
    still need to implement completed writing logic (and formatters)

--- candejar/cidrw/write.py
+++ candejar/cidrw/write.py
@@ -1,51 +1,65 @@
 # -*- coding: utf-8 -*-
 
 """Contains the procedure for writing a `CidObj` to a .cid file."""
-from dataclasses import asdict, is_dataclass, fields
+from itertools import chain, tee, repeat
 from pathlib import Path
-from typing import Iterator, TypeVar, Mapping, Type, Any, Callable, Sequence, Optional
+from typing import Iterator, TypeVar, Mapping, Type, Optional, Tuple
 
 from .exc import CIDRWError
-from ..cid import A2, C3, C4, C5, D1, E1
-from ..cidprocessing.main import process as process_cid
+from ..utilities.dataclasses import unmapify, mapify, field_names
+from ..cid import A1, A2, C1, C2, C3, C4, C5, D1, E1, Stop
 from ..cid import CidLine
 
 SEQ_TYPES = {A2: "pipe_groups", C3: "nodes", C4: "elements", C5: "boundaries", D1: "materials", E1: "factors"}
+NON_SEQ_SUB_TYPES = {A1, C1, C2} | SEQ_TYPES.keys()
 
 CidObj = TypeVar("CidObj")
 CidLineType = Type[CidLine]
 
-def lines(cid: CidObj) -> Iterator[CidLine]:
-    i_line_types: Iterator[CidLineType] = process_cid(cid)
-    yield from process_lines(cid, i_line_types)
-
 def process_lines(cid: CidObj, i_line_types: Iterator[CidLineType]) -> Iterator[CidLine]:
     while True:
-        try:
-            line_type: CidLineType = next(i_line_types)
-            valid_fields: Sequence[str] = [f.name for f in fields(line_type)]
-            target_obj_name: Optional[str] = SEQ_TYPES.get(line_type, "")
-            target_obj = getattr(cid, target_obj_name, cid)
-            if target_obj is cid:
-                target_obj = [target_obj]
-            for subobj in target_obj:
-                d: Mapping = todict(subobj)
-                yield line_type(**{k: v for k, v in d.items() if k in valid_fields})
-        except StopIteration:
-            break
+        line_type = next(i_line_types)
+        valid_fields = field_names(line_type)
+        target_obj_name: Optional[str] = SEQ_TYPES.get(line_type, "")
+        target_obj = getattr(cid, target_obj_name, cid)
+        if target_obj is cid:
+            target_obj = [target_obj]
+        for subobj in target_obj:
+            d: Mapping = mapify(subobj)
+            yield unmapify(d, line_type, lambda k: k in valid_fields)
+            while True:
+                # look ahead in `i_line_types`
+                try:
+                    prev_line_type, line_type = line_type, next(i_line_types)
+                except StopIteration:
+                    if line_type is Stop:
+                        break
+                    else:
+                        raise CIDRWError("Encountered StopIteration before Stop object")
+                if line_type in NON_SEQ_SUB_TYPES:
+                    # put the iterated item back at the front and unswap line_type
+                    i_line_types = chain([line_type], i_line_types)
+                    line_type = prev_line_type
+                    break
+                else:
+                    # line_type is a sub-type; use current subobj to write line
+                    valid_fields = field_names(line_type)
+                    yield unmapify(d, line_type, lambda k: k in valid_fields)
 
-def todict(o: Any) -> Mapping:
-    """Map-ify an object so it can be unpacked as **kwargs to another context."""
-    if is_dataclass(o):
-        return asdict(o)
-    elif isinstance(o, Mapping):
-        return o
-    else:
-        if isinstance(o, type):
-            raise CIDRWError("Received command to turn the class instance of {o.__name__!s} to a mapping")
-        else:
-            return vars(o)
+def lines_w_formatting(cid: CidObj, i_line_types: Iterator[CidLineType]) -> Iterator[Tuple[CidLine, str]]:
+    """The line object and appropriate format code as a tuple: (line_obj, format_str)
 
-def file(cid: CidObj, path: Path):
-    with path.open("w"):
-        path.write_text("\n".join(lines(cid)))
+    The number of objects in A1, C2 are updated to match lengths of sub-object sequences.
+    """
+    i_line_types1, i_line_types2 = tee(i_line_types)
+    i_lines, i_line_types = zip(process_lines(cid, i_line_types1), i_line_types2)
+    # TODO: write logic for this generator
+    yield from zip(i_lines, repeat("cid"))
+
+def line_strings(cid: CidObj, i_line_types: Iterator[CidLineType]) -> Iterator[str]:
+    ilines_w_formatting = lines_w_formatting(cid, i_line_types)
+    yield from (format(o, f) for o,f in ilines_w_formatting)
+
+def file(cid: CidObj, i_line_types, path: Path, mode="x"):
+    with path.open(mode):
+        path.write_text("\n".join(line_strings(cid, i_line_types)))
