fb5e48b07e96e03036a02a48ca46a0af6da8a8d0
Rick Teachey <ricky@teachey.org>
2018-04-26 23:59:57 -0400

    new PipeGroup and Material sub classes (e.g. Plastic, Isotropic, etc)
    completed factories; new class specialty attrs/methods need work
    
    Signed-off-by: Rick Teachey <ricky@teachey.org>

--- candejar/cidobjrw/cidsubobj/cidsubobj.py
+++ candejar/cidobjrw/cidsubobj/cidsubobj.py
@@ -5,12 +5,13 @@ instance returned on the fly.
 """
 
 from types import new_class
-from typing import Generic, Union, TypeVar, Dict
+from typing import Generic, Union, TypeVar, Dict, Any
 
-from ..names import FEA_TYPE_DICT
 from .names import SUB_OBJ_CLASS_DICT
+from ..cidsubobj.names import PIPE_GROUP_CLASS_DICT, SOIL_MATERIAL_DICT
+from ..names import FEA_TYPE_DICT
 from ...utilities.mixins import ChildRegistryBase
-from ...cid import CidSubLine
+from ...cid import CidSubLine, A2, D1
 from ... import fea
 
 
@@ -26,7 +27,7 @@ CidSeq = TypeVar("CidSeq")
 class CidSubObj(ChildRegistryBase, Generic[CidObj, CidSeq, CidSubLine, fea.FEAObj]):
     """A viewer object that gets its data from the `CidLine` objects in `.cid_obj`."""
 
-    def __init__(self, _container: CidSeq, _idx: int, **kwargs: CidData):
+    def __init__(self, _container: CidSeq, _idx: int, **kwargs: CidData) -> None:
         # make ABC
         if type(self)==CidSubObj:
             raise TypeError("Can't instantiate abstract class CidSubObj without line_type attribute")
@@ -37,14 +38,14 @@ class CidSubObj(ChildRegistryBase, Generic[CidObj, CidSeq, CidSubLine, fea.FEAOb
     def _asdict(self) -> Dict[str, CidData]:
         return {k:v for k,v in vars(self).items() if k not in "_container _idx".split()}
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         items = ("{}={!r}".format(k, v) for k,v in vars(self).items() if k not in "_container _idx".split())
         return "{}({})".format(type(self).__name__, ", ".join(items))
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         return self.__repr__() == other.__repr__()
 
-    def __getattribute__(self, name):
+    def __getattribute__(self, name: str) -> Any:
         try:
             return super().__getattribute__(name)
         except AttributeError as e:
@@ -67,10 +68,41 @@ def subclass_CidSubObj(sub_line_type):
     except KeyError:
         pass
 
-    # resolve 2 of the CidSubObj input types
+    # resolve 2 of the CidSubObj generic types
     SubLine = sub_line_type  # type of CidLine indicating start of an object in CID file
     FEA_Obj = FEA_TYPE_DICT[sub_line_type]  # type of FEA object corresponding to CID object
 
     cls = new_class(cls_name, (CidSubObj[CidObj, CidSeq, SubLine, FEA_Obj], Generic[CidObj, CidSeq]))
 
     return cls
+
+def subclass_PipeGroupObj(type_):
+    """Produce a `PipeGroup` based subclass."""
+
+    # see if already exists
+    pipe_group_cls_name = PIPE_GROUP_CLASS_DICT[type_]
+    PipeGroup = subclass_CidSubObj(A2)
+    try:
+        return PipeGroup.subclasses[pipe_group_cls_name]
+    except KeyError:
+        pass
+
+    cls = new_class(pipe_group_cls_name, (PipeGroup,))
+
+    return cls
+
+def subclass_MaterialObj(num):
+    """Produce a `Material` based subclass."""
+
+    # see if already exists
+    material_cls_name = SOIL_MATERIAL_DICT[num]
+    Material = subclass_CidSubObj(D1)
+    try:
+        return Material.subclasses[material_cls_name]
+    except KeyError:
+        pass
+
+    cls = new_class(material_cls_name, (Material,))
+
+    return cls
+
--- candejar/cidobjrw/cidsubobj/names.py
+++ candejar/cidobjrw/cidsubobj/names.py
@@ -7,3 +7,11 @@ from ...cid import SEQ_LINE_TYPES
 
 SUB_OBJ_CLASS_NAMES = ("PipeGroup", "Node", "Element", "Boundary", "Material", "Factor")
 SUB_OBJ_CLASS_DICT = dict(zip(SEQ_LINE_TYPES, SUB_OBJ_CLASS_NAMES))
+
+PIPE_GROUP_TYPE_NAMES = "ALUMINUM, BASIC, CONCRETE, PLASTIC, STEEL, CONRIB, CONTUBE".split(", ")
+PIPE_GROUP_CLASS_NAMES = tuple(s.capitalize() for s in PIPE_GROUP_TYPE_NAMES)
+PIPE_GROUP_CLASS_DICT = dict(zip(PIPE_GROUP_TYPE_NAMES, PIPE_GROUP_CLASS_NAMES))
+
+# only including materials interesting in using
+# TODO: add other materials later
+SOIL_MATERIAL_DICT = {1: "Isotropic", 3: "DuncanSelig", 6: "Interface", 8: "MohrCoulomb"}
