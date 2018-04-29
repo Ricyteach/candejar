70bbf8b386ae8623a62cfda1884d5829125fc33b
Rick Teachey <ricky@teachey.org>
2018-04-29 00:27:57 -0400

    PipeGroup subclass dispatch completed; Basic pipe group created
    Signed-off-by: Rick Teachey <ricky@teachey.org>

--- candejar/candeobj/pipe_groups.py
+++ candejar/candeobj/pipe_groups.py
@@ -13,11 +13,31 @@ from ..utilities.mixins import ChildRegistryBase
 @dataclass
 class PipeGroup(ChildRegistryBase):
     type_: str  # ALUMINUM, BASIC, CONCRETE, PLASTIC, STEEL, CONRIB, CONTUBE
-
+    def __new__(cls, type_, *args, **kwargs):
+        if cls is PipeGroup:
+            pipe_cls = dict(BASIC=Basic, ALUMINUM=Aluminum,
+                            STEEL=Steel, PLASTIC=Plastic)[type_]
+            return super(PipeGroup,pipe_cls).__new__(pipe_cls)
+        else:
+            return super(PipeGroup,cls).__new__(cls)
 
 @dataclass
 class Basic(PipeGroup):
-    pass
+    # B1Basic
+    # ANALYS only
+    # repeatable (multiple properties in one pipe group)
+    first: int = 0
+    last: int = 0
+    modulus: float = 0.0 # psi
+    poissons: float = 0.0
+    area: float = 0.0 # in2/in
+    i: float = 0.0 # in4/in
+    load: float = 0.0 # lbs/in
+
+    # B2Basic
+    # for ANALYS mode only
+    # Small Deformation: 0, Large Deformation: 1, Plus Buckling: 2
+    mode: int = 0
 
 
 @dataclass
