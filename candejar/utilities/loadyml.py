fff68b107ed3ccc5b88855111413bdad9cf0b46c
Rick Teachey <ricky@teachey.org>
2018-05-15 16:23:27 -0400

    load_yml_objs annotations

--- candejar/utilities/loadyml.py
+++ candejar/utilities/loadyml.py
@@ -2,12 +2,12 @@
 
 """Load `ciddefs.yml` file into objects."""
 
-from typing import Union
+from typing import Union, List
 
 import yaml
 from pathlib import Path
 
-def load_yml_objs(path: Union[str,Path]):
+def load_yml_objs(path: Union[str,Path]) -> List:
     with Path(path).open() as f:
         objs = list(yaml.safe_load_all(f))
     return objs
