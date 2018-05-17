0390ad97ff5ecc093a665611c9e7e9ee37a29374
Rick Teachey <ricky@teachey.org>
2018-05-17 10:52:21 -0400

    improved geometry.ops.splitLR function but still not working

--- candejar/geometry/ops.py
+++ candejar/geometry/ops.py
@@ -2,7 +2,7 @@
 
 """Operations for working with geometries."""
 
-from typing import NamedTuple
+from typing import NamedTuple, Tuple
 
 import shapely.geometry as geo
 import shapely.ops as ops
@@ -16,14 +16,32 @@ class SplitGeometry(NamedTuple):
 def splitLR(geom: geo.base.BaseGeometry,
             splitter: geo.base.BaseGeometry) -> SplitGeometry:
     """Split a geometry into a 'left' and 'right' side using the shapely API"""
+    if not isinstance(splitter, geo.LineString):
+        raise GeometryError("The splitter must be a LineString")
     if not splitter.is_simple:
         raise GeometryError("Only simple splitter objects allowed")
+    if hasattr(geom, "__iter__"):
+        raise GeometryError("Geometry collections not allowed")
     geom = geo.GeometryCollection([geom])
     geom_extents = geo.GeometryCollection([*geom, splitter]).minimum_rotated_rectangle
     sides = ops.split(geom_extents, splitter)
     try:
         Aside, Bside = sides
     except TypeError:
-        raise GeometryError("the splitter must extend beyond "
-                            "minimum_rotated_rectangle of geom")
-    return SplitGeometry(side.intersection(geom) for side in (Lside, Rside))
+        # only 1 result - rotated rectangle wasn't split
+        if len(ops.split(geom, splitter)) == 1:
+            # geom isn't split by splitter
+            raise GeometryError("the splitter does not appear to split the geometry")
+        else:
+            # splitter too small for algorithm
+            raise GeometryError("the splitter must extend beyond minimum_rotated_rectangle of the combined geometry")
+    # determine Lside and Rside here
+    Lside, Rside = _get_LRsides(Aside, Bside, splitter)
+    return SplitGeometry(*(side.intersection(geom) for side in (Lside, Rside)))
+
+def _get_LRsides(Aside: geo.base.BaseGeometry,
+                 Bside: geo.base.BaseGeometry,
+                 splitter: geo.base.BaseGeometry) -> Tuple(geo.base.BaseGeometry,
+                                                           geo.base.BaseGeometry):
+    """Determine the 'left' and 'right' sides of an already split geometry"""
+    return Aside,Bside
