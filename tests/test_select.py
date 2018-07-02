7e9aaf0d3289ac78fa431b8944bfdcedaf17f969
Rick Teachey <ricky@teachey.org>
2018-07-02 17:32:45 -0400

    created test_select

---
+++ tests/test_select.py
@@ -0,0 +1,22 @@
+import pytest
+
+from candejar import select
+from dataclasses import make_dataclass
+
+@pytest.fixture
+def test_type():
+    TestType = make_dataclass("TestType", "mat step".split())
+    return TestType
+
+@pytest.fixture
+def has_mats_seq(test_type):
+    seq = list()
+    for i in range(1,4):
+        for j in range(i):
+            obj = test_type(i, i)
+            seq.append(obj)
+    return seq
+
+def test_by_material(has_mats_seq):
+    x = list(select.by_material(has_mats_seq, 1))
+    assert x
