ec7efab241e6eaec94242056706a344db972e35f
Rick Teachey <ricky@teachey.org>
2018-07-16 09:00:32 -0400

    Converters should really actually be good now.

--- candejar/candeobj/converter.py
+++ candejar/candeobj/converter.py
@@ -2,7 +2,6 @@
 
 """Special tools for working with candeobj types."""
 
-from dataclasses import dataclass, field
 from typing import MutableMapping, Iterator, Union, TypeVar, Any, Dict
 import itertools
 
@@ -13,20 +12,19 @@ CandeSeq = TypeVar("CandeSeq", bound=Union[Nodes, Elements])
 SectionSeq = TypeVar("SectionSeq", bound=Union[NodesSection, ElementsSection])
 
 
-@dataclass(eq=False, repr=False)
 class SubConverter(MutableMapping[int, int]):
     """Builds a mapping that turns an old item num attribute into a new one
     based on the order. The new new item num attributed begins at the starting
     point, start.
     """
-    start: int
-    seq: SectionSeq
-    _d: Dict[int, int] = field(init=False)
 
-    def __post_init__(self) -> None:
+    def __init__(self, start: int, seq: SectionSeq) -> None:
+        self.start = start
+        self.seq = seq
+
         seen = set()
         keys = [(num, seen.add(num))[0] for num in self.nums if num not in seen]
-        self._d = dict(zip(keys, itertools.count(self.start)))
+        self._d: Dict[int, int] = dict(zip(keys, itertools.count(self.start)))
 
     def __getitem__(self, k: int) -> int:
         try:
@@ -62,18 +60,12 @@ class SubConverter(MutableMapping[int, int]):
         yield from (i.num for i in self.seq)
 
 
-@dataclass(eq=False, repr=False)
 class NumConverter(MutableMapping[int, SubConverter]):
     """Builds a mapping of SubConverter objects assigned to each section"""
-    type_: str = field(init=False)
-    seq: CandeSeq = field(repr=False)
-    seq_id: int = field(init=False)
-    _d: Dict[int, SubConverter] = field(init=False, repr=False)
 
-    def __post_init__(self):
-        self.type_ = type(self.seq).__qualname__
-        self.seq_id = id(self.seq)
-        self._d = dict()
+    def __init__(self, seq: CandeSeq):
+        self.seq = seq
+        self._d: Dict[int, SubConverter] = dict()
 
         start = 1
         for section_seq in self.seq.seq_map.values():
@@ -84,18 +76,18 @@ class NumConverter(MutableMapping[int, SubConverter]):
         try:
             return self._d.__getitem__(k)
         except KeyError:
-            if k in self.names:
+            if k in self.ids:
                 raise exc.CandeKeyError(f"conversion mapping missing for section name {k!s}")
             raise exc.CandeKeyError(f"section name {k!s} does not appear in {type(self.seq).__qualname__}")
 
     def __setitem__(self, k: int, v: SubConverter) -> None:
-        if k not in self._d.keys() and k not in self.names:
+        if k not in self._d.keys() and k not in self.ids:
             raise exc.CandeKeyError(f"can only change existing keys - {k!s} not in sections;")
         else:
             self._d.__setitem__(k, v)
 
     def __delitem__(self, k: int) -> None:
-        if k in self.names:
+        if k in self.ids:
             raise exc.CandeKeyError(f"section name {k!s} must first be removed from {type(self.seq).__qualname__}")
         self._d.__delitem__(k)
 
@@ -106,13 +98,24 @@ class NumConverter(MutableMapping[int, SubConverter]):
         return self._d.__len__()
 
     def __repr__(self) -> str:
-        return f'NumConverter(type_={self.type_!r}, seq_len={self.seq_len!r}, seq_id={self.seq_id!r})'
+        return f'NumConverter(seq_type={self.seq_type!r}, seq_len={self.seq_len!r}, seq_id={self.seq_id!r})'
+
+    def by_name(self, name: str):
+        return self._d[id(self.seq[name])]
+
+    @property
+    def seq_id(self) -> int:
+        return id(self.seq)
 
     @property
     def seq_len(self) -> int:
         return len(self.seq)
 
     @property
-    def names(self) -> Iterator[Any]:
+    def seq_type(self) -> str:
+        return type(self.seq).__qualname__
+
+    @property
+    def ids(self) -> Iterator[Any]:
         """The current section names for the referenced CANDE item sequence"""
-        yield from self.seq.seq_map.keys()
+        yield from (id(v) for v in self.seq.seq_map.values())
