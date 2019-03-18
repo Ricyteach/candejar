1955d391466b058b1572bb163008ad4130798c44
Rick Teachey <ricky@teachey.org>
2019-03-18 17:10:26 -0400

    expanded test_api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,17 +1,26 @@
-import yaml
-from io import StringIO
+import pytest
+import candejar.cande as cande
 
-from candejar import CandeObj
-from candejar.cidl3 import cidl3_dict_to_candeobj_dict
+''' Tests the API against a simple input problem (illustrated below).
+
+SOIL SECTION    STRUCT SECTION      COMBINED SECTIONS   MATED SECTIONS
+ _     _         _                   _  _  _             _ _ _
+|_|   |_|       |_|                 |_||_||_|           |_|_|_|
+'''
 
 SOIL = '''
-4
+8
 1    0.0    0.0
 2    1.0    0.0
 3    1.0    1.0
 4    0.0    1.0
-1
+5    2.0    0.0
+6    3.0    0.0
+7    3.0    1.0
+8    2.0    1.0
+2
 1    1    2    3    4
+2    5    6    7    8
 '''[1:-1]
 
 STRUCT = '''
@@ -92,8 +101,87 @@ SoilMaterials:
         Poissons: 0.3
 '''[1:-1]
 
-def test_api():
-    cobj = CandeObj(**cidl3_dict_to_candeobj_dict(yaml.safe_load(StringIO(CIDL3))))
-    cobj.add_from_msh(StringIO(SOIL), name="SOIL")
-    cobj.add_from_msh(StringIO(STRUCT), name="STRUCT")
 
+@pytest.fixture
+def cidl3_path(tmp_path):
+    """Make the CIDL3 input file"""
+    path = tmp_path / "test_api.cidl3"
+    path.write_text(CIDL3)
+    return path
+
+
+@pytest.fixture
+def soil_msh_path(tmp_path):
+    """Make the soil msh input file"""
+    path = tmp_path / "soil.msh"
+    path.write_text(SOIL)
+    return path
+
+
+@pytest.fixture
+def structure_msh_path(tmp_path):
+    """Make the structure msh input file"""
+    path = tmp_path / "structure.msh"
+    path.write_text(STRUCT)
+    return path
+
+
+class TestAPI:
+    @pytest.fixture
+    def candejar_obj(self, cidl3_path):
+        """Fresh CandeObj to use for API tests"""
+        return cande.open(cidl3_path)
+
+    @pytest.fixture
+    def candejar_obj_with_msh_added(self, candejar_obj, soil_msh_path, structure_msh_path):
+        candejar_obj.add_from_msh(soil_msh_path, name="SOIL")
+        candejar_obj.add_from_msh(structure_msh_path, name="STRUCT")
+        return candejar_obj
+
+    def test_msh_info(self, candejar_obj_with_msh_added):
+        assert tuple(getattr(candejar_obj_with_msh_added.nodes["SOIL"][0], attr)
+                     for attr in "num x y master".split()) == (1, 0.0, 0.0, None)
+        assert tuple(getattr(candejar_obj_with_msh_added.nodes["STRUCT"][0], attr)
+                     for attr in "num x y master".split()) == (1, 1.0, 0.0, None)
+        assert tuple(getattr(candejar_obj_with_msh_added.elements["SOIL"][0], attr)
+                     for attr in "i j k l mat step".split()) == (1, 2, 3, 4, 0, 0)
+        assert tuple(getattr(candejar_obj_with_msh_added.elements["STRUCT"][0], attr)
+                     for attr in "i j k l mat step".split()) == (2, 1, 1, 1, 0, 0)
+        assert len(candejar_obj_with_msh_added.nodes) == 12
+        # check direct iteration:
+        assert sum(1 for _ in candejar_obj_with_msh_added.nodes) == 12
+        #assert candejar_obj_with_msh_added.boundaries["SOIL"]
+        #assert candejar_obj_with_msh_added.boundaries["STRUCT"]
+
+    @pytest.fixture
+    def candejar_obj_mated(self, candejar_obj_with_msh_added):
+        """Establish a mated connection between sections"""
+        sections = tuple(candejar_obj_with_msh_added.elements[k] for k in "SOIL STRUCT".split())
+        candejar_obj_with_msh_added.mate_sections(*sections)
+        return candejar_obj_with_msh_added
+
+    def test_mated_sections(self, candejar_obj_mated):
+        assert len(candejar_obj_mated.connections) == 4
+
+    @pytest.fixture
+    def candejar_obj_standard_boundaries(self, candejar_obj_mated):
+        candejar_obj_mated.add_standard_boundaries(nodes=candejar_obj_mated.nodes["SOIL"])
+        return candejar_obj_mated
+
+    def test_standard_boundaries(self, candejar_obj_standard_boundaries):
+        assert len(candejar_obj_standard_boundaries.boundaries) == 6
+
+    @pytest.fixture
+    def candejar_obj_ready(self, candejar_obj_standard_boundaries):
+        candejar_obj_standard_boundaries.prepare()
+        return candejar_obj_standard_boundaries
+
+    def test_prepared(self, candejar_obj_ready):
+        """After mated connections, some master nodes are set to a node in the other mated section"""
+        assert candejar_obj_ready.nodes["STRUCT"][0].master is candejar_obj_ready.nodes["SOIL"][1]
+        assert candejar_obj_ready.nodes["STRUCT"][3].master is candejar_obj_ready.nodes["SOIL"][2]
+        assert len(candejar_obj_ready.nodes) == 12
+        # bypass direct iteration with len():
+        assert sum(1 for _ in range(len(candejar_obj_ready.nodes))) == 12
+        assert sum(1 for num in range(len(candejar_obj_ready.nodes)) if candejar_obj_ready.nodes[num].master is None) == 8  # slave nodes skipped
+        assert sum(1 for _ in candejar_obj_ready.nodes["STRUCT"]) == 0  # slave nodes not iterated
